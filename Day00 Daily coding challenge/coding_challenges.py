35187d368ed2d91b4d9b49d75f4a633744fafde0
graciari2000 <92252649+graciari2000@users.noreply.github.com>
2023-08-30 21:24:46 +0100

    day00

---
+++ Day00 Daily coding challenge/coding_challenges.py
@@ -0,0 +1,13 @@
+import random
+
+def daily_challenge():
+    with open("./projects.txt") as projects_file:
+        project_lines = projects_file.readlines()
+        # choose a random line from the file
+        project_line = random.choice(project_lines)
+    
+    with open("./languages.txt") as languages_file:
+        language_lines = languages_file.readlines()
+        language_line = random.choice(language_lines)
+    
+    print(f"Hello Kafui! Today's project is '{project_line.strip()}' in {language_line.strip()}.")
