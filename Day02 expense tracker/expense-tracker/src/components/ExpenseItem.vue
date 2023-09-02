7b74a68be96ad297cf3174cea73ad3aba8873eee
graciari2000 <ariane-gracia.akakpo@epitech.eu>
2023-09-02 18:46:12 +0100

    expene tracker redo because of a bad installation

---
+++ Day02 expense tracker/expense-tracker/src/components/ExpenseItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <h2>Expense Tracker App</h2>
+    <p>Welcome to the Expense Tracker App</p>
+  <div class="expense-item">
+    <h3>{{ expense.name }}</h3>
+    <p>Amount: ${{ expense.amount }}</p>
+    <p>Date: {{ expense.date }}</p>
+    <p>Category: {{ expense.category }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    expense: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.expense-item {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f9f9f9;
+}
+</style>
