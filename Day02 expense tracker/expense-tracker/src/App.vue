95a495cc8c93a856677938f63623e33491322bea
graciari2000 <ariane-gracia.akakpo@epitech.eu>
2023-09-02 18:47:48 +0100

    expense tracker redo because of a bad installation

---
+++ Day02 expense tracker/expense-tracker/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>Expense Tracker</h1>
+    <div>
+      <h2>Expense List</h2>
+      <!-- Use the ExpenseItem component here -->
+      <expense-item :expense="expenseData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ExpenseItem from './components/ExpenseItem.vue'; // Adjust the import path as needed
+
+export default {
+  components: {
+    ExpenseItem, // Register the ExpenseItem component
+  },
+  data() {
+    return {
+      expenseData: {
+        name: 'Groceries',
+        amount: 50.00,
+        date: '2023-09-02',
+        category: 'Food',
+      },
+    };
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
