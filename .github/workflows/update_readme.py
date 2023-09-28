0cdfb093f52d75f6ef5a2ea9eb8f8fec18c57ceb
graciari2000 <92252649+graciari2000@users.noreply.github.com>
2023-09-29 00:21:00 +0100

    python script

--- .github/workflows/update_readme.py
+++ .github/workflows/update_readme.py
@@ -18,67 +18,35 @@ current_day = (datetime.now() - datetime(2023, 9, 27)).days + 1  # Adjust the st
 progress_entry = f"### Day {current_day} ({current_date}):\n\n- [Link to Day {current_day}](Day{current_day}/Day{current_day}.md)\n"
 
 # Fetch the list of repository contents using the GitHub API
-url = f"https://api.github.com/repos/graciari2000/100daysofcode/"
-response = requests.get(url, headers={"Authorization": f"token {access_token}"})
+url = f"https://api.github.com/repos/{github_username}/{repository_name}/contents/{readme_path}"
+headers = {"Authorization": f"token {access_token}"}
+response = requests.get(url, headers=headers)
 
-# Check if the request was successful
 if response.status_code == 200:
-    contents = response.json()
-    
-    # Check if the Day{current_day} directory already exists in the repository
-    day_directory_exists = any(content["name"] == f"Day{current_day}" for content in contents)
-    
-    if not day_directory_exists:
-        # Create the Day{current_day} directory
-        create_day_directory_url = f"https://api.github.com/repos/{github_username}/{repository_name}/contents/Day{current_day}"
-        create_day_directory_data = {
-        "message": f"Create Day{current_day} directory",
-        "content": "",
-        }
-        create_day_directory_response = requests.put(
-            create_day_directory_url,
-            headers={"Authorization": f"token {access_token}"},
-            json=create_day_directory_data,
-        )
-        
-        # Check if the directory creation was successful
-    if create_day_directory_response.status_code == 201:
-        print(f"Created Day{current_day} directory.")
+    # Decode the content from base64
+    content = response.json()
+    current_readme_content = content["content"]
+    current_readme_content = base64.b64decode(current_readme_content).decode("utf-8")
+
+    # Update the README content
+    updated_readme_content = progress_entry + "\n" + current_readme_content
+
+    # Encode the updated content to base64
+    updated_readme_content_base64 = base64.b64encode(updated_readme_content.encode("utf-8")).decode("utf-8")
+
+    # Create a commit to update the README
+    update_data = {
+        "message": f"Update progress for Day {current_day}",
+        "content": updated_readme_content_base64,
+        "sha": content["sha"]
+    }
+
+    update_url = f"https://api.github.com/repos/{github_username}/{repository_name}/contents/{readme_path}"
+    update_response = requests.put(update_url, headers=headers, json=update_data)
+
+    if update_response.status_code == 200:
+        print(f"Updated README for Day {current_day}")
     else:
-        print(f"Failed to create Day{current_day} directory.")
-    
-    # Read the existing README content
-    with open(readme_path, "r") as f:
-        readme_content = f.read()
-
-    # Append the progress entry to the README content
-    updated_readme_content = progress_entry + "\n" + readme_content
-
-    # Write the updated content back to the README
-    with open(readme_path, "w") as f:
-        f.write(updated_readme_content)
-
-    # Commit and push the changes
-    os.system("git config --global user.name 'Your Name'")
-    os.system("git config --global user.email 'youremail@example.com'")
-    os.system(f"git add {readme_path}")
-    os.system(f"git commit -m 'Add progress update for Day {current_day}'")
-    os.system("git push")
+        print(f"Failed to update README for Day {current_day}")
 else:
-    print("Failed to fetch repository contents.")
-    
-with open(readme_path, "r") as f:
-    readme_content = f.read()
-    updated_readme_content = progress_entry + "\n" + readme_content
-
-with open(readme_path, "w") as f:
-    f.write(updated_readme_content)
-
-os.system("git config --global user.name 'graciari2000'")
-os.system("git config --global user.email 'youremail@example.com'")
-os.system(f"git add {readme_path}")
-os.system(f"git commit -m 'Add progress update for Day {current_day}'")
-os.system("git push")
-
-    else:
-        print("Failed to fetch repository contents.")
+    print("Failed to fetch README content from the repository.")
